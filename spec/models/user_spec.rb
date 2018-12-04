be92afd4d2598c62fd1a01cf26a47ea2f72c827d
normanrs <normanrs@live.com>
2018-12-04 16:29:09 -0700

    new key

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe User, type: :model do
     it 'can be store a users github api key' do
       norm = User.create(email: 'norm@email.com', password: 'norm', first_name:'Norm', role: 0)
       otherguy = User.create(email: 'dude@email.com', password: 'dude', first_name:'dude', role: 0)
-      normapi = Apikey.create!(user_id: norm.id, host: 0, key: "token 534e41060850a8305ff5a92147a8c7a32fe0cb61")
+      normapi = Apikey.create!(user_id: norm.id, host: 0, key: "token blahblahblahblah")
       Apikey.create!(user_id: otherguy.id, host: 0, key: "token blahblahblahblahblah")
       expect(norm.github_key).to eq(normapi[:key])
     end
