55a048bb6f9b0a4e1647db194f8543fe22884e7f
normanrs <normanrs@live.com>
2018-12-05 15:27:39 -0700

    added follower functionality with passing tests

---
+++ spec/features/user/user_can_see_followers_in_dashboard_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe 'User followers' do
+	it 'shown in dashboard' do
+		stub_repo_api_calls
+		stub_follower_api_calls
+		key = create(:apikey)
+		allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(key.user)
+
+		visit dashboard_path
+
+		expect(page).to have_content("Followers")
+		expect(page).to have_css(".follower", count: 15)
+
+		within(".followers") do
+			expect(page).to have_css(".follower-name", count:15)
+			expect(page).to have_link(count: 15)
+		end
+	end
+
+	xit "only shows github section if user has API key" do
+		stub_repo_api_calls
+		norm = User.create(email: 'norm@email.com', password: 'norm', first_name:'Norm', role: 0)
+		otherguy = User.create(email: 'dude@email.com', password: 'dude', first_name:'dude', role: 0)
+		normapi = Apikey.create!(user_id: norm.id, host: 0, key: "token blahblahblahblah")
+
+		allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(otherguy)
+
+		visit dashboard_path
+		expect(page).to have_content(otherguy.first_name)
+
+		expect(page).to_not have_css(".github")
+	end
+
+end
