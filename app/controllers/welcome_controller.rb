2b1b4f25c47bf012342e00724cca5df4e8217905
normanrs <normanrs@live.com>
2018-12-11 19:28:49 -0700

    tested new functionality of differentiating between public and private tutorials

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,18 @@
 class WelcomeController < ApplicationController
   def index
-    if params[:tag]
-      @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
+    if current_user != nil
+      if params[:tag]
+        @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
+      else
+        @tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
+      end
+
     else
-      @tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
+      if params[:tag]
+        @tutorials = Tutorial.public_tutorials.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
+      else
+        @tutorials = Tutorial.public_tutorials.paginate(:page => params[:page], :per_page => 5)
+      end
     end
   end
 end
